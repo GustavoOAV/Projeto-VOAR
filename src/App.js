7b05a745a4c7c7863cc2e17c0e7d978d83acea11
Gustavo Orlando <gudmx100@hotmail.com>
2025-09-02 13:26:03 -0300

    Sis de Avaliação Incompleto

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Carrosel from "./components/Carrosel.js";
 import Critica from "./components/Critica.js";
 import Elenco from "./components/Elenco.js";
 import Espetaculo from "./components/Espetaculo.js";
-import Feedback from "./components/Feedback.js";
+
 import Galeria from "./components/Galeria.js";
 import Navbar from "./components/Navbar.js";
 
@@ -16,7 +16,7 @@ function App() {
      <Elenco />
      <Critica />
      <Galeria />
-     <Feedback />
+     
     </div>
   );
 }
